fe4f818e17a06c15c00268dedcaca351dd9a9d93
geogit2024 <ryanposgeo@gmail.com>
2026-05-03 19:44:25 -0300

    fix: allow local http api proxy

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          // Prevent mixed-content failures by upgrading accidental http:// subrequests to https://.
-          { key: "Content-Security-Policy", value: "upgrade-insecure-requests" },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
